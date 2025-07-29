e0f3bd860ae5e89dacaef37726d1f04446afc30f
Lek <ailecleon8@gmail.com>
2025-07-29 11:41:40 -0600

    refactor: simplify hotmartClubURL usage and improve layout in PurchaseCard component

--- components/purchase-card.tsx
+++ components/purchase-card.tsx
@@ -2,14 +2,17 @@
 
 import { Card, CardContent } from "@/components/ui/card";
 import { Purchase } from "../types/purchase";
+import Link from "next/link";
+import { ChevronsRight } from "lucide-react";
 
 interface PurchaseCardProps {
   purchase: Purchase;
 }
 
 export function PurchaseCard({ purchase }: PurchaseCardProps) {
+  const hotmartClubURL = process.env.NEXT_PUBLIC_HOTMART_CLUB_URL;
   const handleSelectedPurchase = (purchaseId: number) => {
-    window.open(process.env.NEXT_PUBLIC_HOTMART_CLUB_URL || "", "_blank");
+    window.open(hotmartClubURL || "", "_blank");
   };
   const date = new Date(purchase.created_at).toLocaleDateString();
 
@@ -25,9 +28,19 @@ export function PurchaseCard({ purchase }: PurchaseCardProps) {
           <span className="text-sm text-gray-500">{date}</span>
         </div>
         <hr className="my-2" />
-        <span className="text-sm text-gray-400 font-semibold">
-          {purchase?.currency} {purchase?.realPrice.toFixed(2)}{" "}
-        </span>
+        <div className="flex justify-between items-center gap-8">
+          <span className="text-sm text-gray-400 font-semibold">
+            {purchase?.currency} {purchase?.realPrice.toFixed(2)}{" "}
+          </span>
+          <Link
+            className="font-semibold text-primary-dark flex items-center me-2transition-all duration-200"
+            href={hotmartClubURL || "/home"}
+            target="_blank"
+          >
+            Ir al curso
+            <ChevronsRight className="ml-1 mt-[3px]" size={21} />
+          </Link>
+        </div>
       </CardContent>
     </Card>
   );
